83aec6421288197d587cace0af6048116f4d653b
Lily <20535390+kitten-lily@users.noreply.github.com>
2025-07-08 21:17:44 +0200

    Use op-cli container

--- README.md
+++ README.md
@@ -25,18 +25,18 @@ Tested on Fedora 41, Ubuntu 24.04, and CentOS Stream 9/derivatives.
 
 ### 1. Create required directories
 ```bash
-sudo mkdir -p /var/lib/orches /etc/containers/systemd
+mkdir -p /var/lib/orches /etc/containers/systemd
 ```
 
 ### 2. Initialize orches with this repository
 ```bash
-sudo podman run --rm -it --pid=host --pull=newer \
+podman run --rm -it --pid=host --pull=newer \
   --mount \
     type=bind,source=/run/systemd,destination=/run/systemd \
   -v /var/lib/orches:/var/lib/orches \
   -v /etc/containers/systemd:/etc/containers/systemd  \
   ghcr.io/orches-team/orches init \
-  https://github.com/orches-team/orches-config-rootful.git
+  https://github.com/kitten-lily/orches-pangolin.git
 ```
 
 ### 3. Verify orches and caddy are running
--- scripts/utils.sh
+++ scripts/utils.sh
@@ -2,24 +2,17 @@
 
 TOKEN_FILE="/var/lib/orches/.op-token"
 VAULT="${HOSTNAME^}"
-OP=/home/linuxbrew/.linuxbrew/bin/op
+OP="podman run --rm --secret op-sa-token,type=env,target=OP_SERVICE_ACCOUNT_TOKEN docker.io/1password/op:2 op"
 
 # Check if 1Password Service Account Token is set
 check_op_token() {
-  # Verify that the 1Password Service Account Token is set
-  if [[ ! -f $TOKEN_FILE ]]; then
-    echo "ERROR: $TOKEN_FILE does not exist."
-    echo "Please run the op_token function to create it."
-    exit 1
+  if podman secret exists op-sa-token; then
+    echo "token exists"
+    return 0
   fi
 
-  . $TOKEN_FILE
-
-  if [[ -z "${OP_SERVICE_ACCOUNT_TOKEN}" ]]; then
-    echo "ERROR: OP_SERVICE_ACCOUNT_TOKEN environment variable is not set"
-    echo "Please use the op_token function to set it up."
-    exit 1
-  fi
+  echo "ERROR: podman secret op-sa-token must be set to your service account token"
+  exit 1
 }
 
 # Create podman secret from 1Password item
@@ -43,19 +36,4 @@ podman_secret() {
   $OP item get "$ITEM" --vault "$VAULT" --fields "$FIELD" --reveal --format json | \
   echo -n $(jq -r '.value') | \
   podman secret create "$SECRET_NAME" - > /dev/null
-}
-
-# Function to set up Newt secrets for a service
-newt_secrets() {
-  local SERVICE="$1"
-
-  if [[ -z "$SERVICE" ]]; then
-    echo "ERROR: Service name is required."
-    echo "Usage: newt_secrets SERVICE"
-    return 1
-  fi
-
-  podman_secret "${SERVICE}" "Pangolin Endpoint" "${SERVICE}-pangolin-endpoint" || return 1
-  podman_secret "${SERVICE}" "Newt ID" "${SERVICE}-newt-id" || return 1
-  podman_secret "${SERVICE}" "Newt Secret" "${SERVICE}-newt-secret" || return 1
 }
