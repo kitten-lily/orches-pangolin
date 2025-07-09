370a81f3e6ae788647d8ff5a320ffa32378d7931
Lily <20535390+kitten-lily@users.noreply.github.com>
2025-07-09 20:38:36 +0200

    TEST

--- pangolin/pangolin_setup.sh
+++ pangolin/pangolin_setup.sh
@@ -5,5 +5,8 @@
 podman_secret Pangolin "Admin E-mail" pangolin-admin-email
 podman_secret Pangolin "Admin Password" pangolin-admin-password
 check_op_token
-$OP_PODMAN --volume /var/lib/orches/repo/pangolin:/pangolin:ro,z $OP_IMAGE op inject --in-file /pangolin/templates/config.yml \
-  > /var/lib/orches/repo/pangolin/config/config.yml
+mkdir /var/tmp/pangolin
+$OP_PODMAN --volume /var/lib/orches/repo/pangolin:/pangolin:ro,z --volume /var/tmp/pangolin:/pangolin/tmp:z $OP_IMAGE op inject --in-file /pangolin/templates/config.yml \
+  --out-file /pangolin/tmp/config.yml
+mv /var/tmp/pangolin/config.yml /var/lib/orches/repo/pangolin/config/config.yml
+rm -r /var/tmp/pangolin
