55b5b04e334a53f7ae98d016d2aeca86771f7015
Lily <20535390+kitten-lily@users.noreply.github.com>
2025-07-10 07:26:25 +0200

    Force

--- pangolin/pangolin_setup.sh
+++ pangolin/pangolin_setup.sh
@@ -7,4 +7,4 @@ podman_secret Pangolin "Admin Password" pangolin-admin-password
 check_op_token
 mkdir /var/tmp/pangolin
 $OP_PODMAN --user root --volume /var/lib/orches/repo/pangolin:/pangolin:ro,z --volume /var/tmp/pangolin:/pangolin/tmp:z $OP_IMAGE op inject --in-file /pangolin/templates/config.yml \
-  --out-file /pangolin/tmp/config.yml
+  --out-file /pangolin/tmp/config.yml --force
