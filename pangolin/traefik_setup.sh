dc0f07568a25fd439618ce4ff715cc2601e4f5d1
Lily <20535390+kitten-lily@users.noreply.github.com>
2025-07-13 17:20:04 +0200

    Add missing directories

--- pangolin/traefik_setup.sh
+++ pangolin/traefik_setup.sh
@@ -2,5 +2,8 @@
 
 . /var/lib/orches/repo/scripts/utils.sh
 
+mkdir -p /var/lib/orches/repo/pangolin/config/traefik/logs
+mkdir -p /var/lib/orches/repo/pangolin/config/letsencrypt
+
 podman_secret Pangolin "Cloudflare Token" pangolin-cloudflare-token
 podman_secret Pangolin "Domain Name" pangolin-domain-name
